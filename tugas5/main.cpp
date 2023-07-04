81c50b4dd79a3e82af4ec6bf2b51721e902d1b4e
arsyaxyz <113373914+arsyaxyz@users.noreply.github.com>
2023-07-05 05:32:53 +0700

    Add files via upload

---
+++ tugas5/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+
+struct mobil
+{
+    string merk;
+    string type;
+    int tahun_pembuatan;
+    float harga;
+};
+
+int main(){
+    mobil *mobil_ptr = nullptr;
+    mobil mobil_x;
+    mobil_ptr = &mobil_x;
+
+    (*mobil_ptr).merk = "Toyota";
+    (*mobil_ptr).type = "mobil sport";
+    (*mobil_ptr).tahun_pembuatan = 2022;
+    (*mobil_ptr).harga = 25000000;
+
+    cout << "Merek : " << (*mobil_ptr).merk << endl;
+    cout << "Type  : " << (*mobil_ptr).type << endl;
+    cout << "Tahun : " << (*mobil_ptr).tahun_pembuatan << endl;
+    cout << fixed << setprecision(0) << "Harga : " << (*mobil_ptr).harga << endl;
+
+
+    return 0;
+}
